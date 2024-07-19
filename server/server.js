7894dc06653daa02d48f727939fa21d8dc7b35cf
chico <chico@gmaiil.com>
2024-07-19 17:02:54 +0800

    modify_server

--- server/server.js
+++ server/server.js
@@ -1,70 +1,50 @@
-import express from 'express';
-import http from 'http';
-import { Server, Socket } from 'socket.io';
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
     cors: {
         origin: '*'
     }
 });
-
-interface User {
-    name: string,
-    id: string
-}
-interface Room {
-    name: string;
-    id: string;
-    players: User[];
-    gameState: GameState | null;
-}
-
-interface GameState {
-    board: string[];
-    currentPlayer: string;
-}
-
-const rooms: Room[] = [];
-const users: Record<string, string> = {};
-
-const checkWin = (board: string[], turn: string): boolean => {
+const rooms = [];
+const users = {};
+const checkWin = (board, turn) => {
     const winConditions = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]             // Diagonals
+        [0, 4, 8], [2, 4, 6] // Diagonals
     ];
-
-    return winConditions.some(([a, b, c]) =>
-        board[a] === turn && board[b] === turn && board[c] === turn
-    );
+    return winConditions.some(([a, b, c]) => board[a] === turn && board[b] === turn && board[c] === turn);
 };
-
-io.on('connection', (socket: Socket) => {
-    socket.on('login', (username: string) => {
+io.on('connection', (socket) => {
+    socket.on('login', (username) => {
         if (Object.values(users).includes(username)) {
             socket.emit('errorMessage', 'User already logged in');
-        } else {
+        }
+        else {
             users[socket.id] = username;
             socket.emit('loginSuccess', username);
             io.emit('rooms', rooms);
         }
     });
-
-    socket.on('createRoom', (roomName: string) => {
+    socket.on('createRoom', (roomName) => {
         if (!users[socket.id]) {
             socket.emit('errorMessage', 'You must be logged in to create a room');
             return;
         }
-
-        const room = rooms.find((room) => room.name === roomName)
-
+        const room = rooms.find((room) => room.name === roomName);
         if (room) {
             socket.emit('errorMessage', 'Rooms name already exist');
             return;
         }
-        const newRoom: Room = {
+        const newRoom = {
             name: roomName,
             id: `room-${Date.now()}`,
             players: [],
@@ -73,8 +53,7 @@ io.on('connection', (socket: Socket) => {
         rooms.push(newRoom);
         io.emit('rooms', rooms);
     });
-
-    socket.on('joinRoom', (roomId: string) => {
+    socket.on('joinRoom', (roomId) => {
         const room = rooms.find(r => r.id === roomId);
         if (!room) {
             socket.emit('errorMessage', 'Room does not exist');
@@ -96,8 +75,7 @@ io.on('connection', (socket: Socket) => {
         }
         io.emit('rooms', rooms);
     });
-
-    socket.on('makeMove', ({ roomId, index }: { roomId: string; index: number }) => {
+    socket.on('makeMove', ({ roomId, index }) => {
         const room = rooms.find(r => r.id === roomId);
         if (!room) {
             socket.emit('errorMessage', 'Room does not exist');
@@ -131,7 +109,8 @@ io.on('connection', (socket: Socket) => {
                 userId: socket.id
             });
             room.gameState = null;
-        } else if (gameState.board.every(cell => cell !== '')) {
+        }
+        else if (gameState.board.every(cell => cell !== '')) {
             io.to(roomId).emit('gameOver', {
                 type: 'draw',
             });
@@ -139,8 +118,7 @@ io.on('connection', (socket: Socket) => {
         }
         io.emit('rooms', rooms);
     });
-
-    socket.on('exitRoom', (roomId: string) => {
+    socket.on('exitRoom', (roomId) => {
         const roomIndex = rooms.findIndex(r => r.id === roomId);
         if (roomIndex === -1) {
             return;
@@ -150,14 +128,14 @@ io.on('connection', (socket: Socket) => {
         io.to(roomId).emit('user-leave', users[socket.id]);
         if (room.players.length === 0) {
             // rooms.splice(roomIndex, 1);
-        } else {
+        }
+        else {
             room.gameState = null;
             io.to(roomId).emit('gameState', null);
         }
         socket.leave(roomId);
         io.emit('rooms', rooms);
     });
-
     socket.on('disconnect', () => {
         for (const roomId in rooms) {
             const room = rooms[roomId];
@@ -167,13 +145,10 @@ io.on('connection', (socket: Socket) => {
         io.emit('rooms', Object.values(rooms));
     });
 });
-
 const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+    res.send('Hello World!');
+});
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
